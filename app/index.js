155c9e15b4a42091fb7eac6255cf926326139a2b
tomymolina <tm07081995@gmail.com>
2015-05-04 19:32:24 +0200

    Solved an error with the camelized app name in the main generator

--- app/index.js
+++ app/index.js
@@ -58,7 +58,7 @@ var MeanGenerator = yeoman.generators.Base.extend({
 
 			this.slugifiedAppName = this._.slugify(this.appName);
 			this.humanizedAppName = this._.humanize(this.appName);
-            this.camelizedAppName = this._.camelCase(this.appName);
+            this.camelizedAppName = this._.camelize(this.appName);
 			this.capitalizedAppAuthor = this._.capitalize(this.appAuthor);
 
 			done();

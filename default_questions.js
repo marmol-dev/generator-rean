f030d6c56fc73d95f2b63a4455256c4d1e50e19a
tomymolina <tm07081995@gmail.com>
2015-05-05 15:30:20 +0200

    Added the rest of the Thinky Schema attribute properties

--- default_questions.js
+++ default_questions.js
@@ -1,68 +1,137 @@
 'use strict';
 
-module.exports = [
-    {
-        name: 'type',
-        default: 'string',
-        type: 'list',
-        required: true,
-        choices: ['string', 'number', 'date', 'object', 'array', 'boolean'],
-        message: 'Which type is your attribute "%name%"?'
-    },
-    {
-        name: 'required',
-        default: false,
-        type: 'confirm',
-        message: 'Is the attribute %name% required?'
-    },
-    {
-        name: 'default',
-        type: 'input',
-        prerequisites: function(val, responses){
-            return (responses.type === 'string' || responses.type === 'number' || responses.type === 'date') && responses.required === false;
-        },
-        casts: ['date', 'number', 'string'],
-        message: 'What is the default value of "%name%"?',
-        postrequisites : function(val) {
-            if (typeof val === 'string') {
-                return val.length > 0;
-            }
+var _ = require('lodash');
 
-            return true;
-        }
+function or(propertyName, values) {
+	return function (value, props) {
+		return values.lastIndexOf(props[propertyName]) > -1;
+	};
+}
+
+module.exports = [
+	{
+		name: 'type',
+		default: 'string',
+		type: 'list',
+		required: true,
+		choices: ['string', 'number', 'date', 'object', 'array', 'boolean'],
+		message: 'Which "type" is your attribute "%name%"?'
     },
-    {
-        name: 'default',
-        type: 'confirm',
-        default: true,
-        prerequisites: {
-            type: 'boolean',
-            required: false
-        },
-        casts: ['boolean'],
-        message: 'What is the default value of "%name%"?'
+	{
+		name: 'required',
+		default: false,
+		type: 'confirm',
+		message: 'Is the attribute "%name%" "required"?'
     },
-    {
-        name: 'min',
-        type: 'input',
-        prerequisites: {
-            'type': 'string'
-        },
-        postrequisites: function (val) {
-            return val > 0;
-        },
-        required: false,
-        casts: ['integer']
+	{
+		name: 'default',
+		prerequisites: {
+			'!type': 'boolean',
+			required: false
+		},
+		casts: ['date', 'number', 'string'],
+		message: 'What is the "default" value of "%name%"?',
+		postrequisites: function (val, params) {
+			if (typeof params.type === 'string') {
+				return val.length > 0;
+			}
+
+			return true;
+		}
+    },
+	{
+		name: 'default',
+		type: 'confirm',
+		default: true,
+		prerequisites: {
+			type: 'boolean',
+			required: false
+		},
+		casts: ['boolean'],
+		message: 'What is the "default" value of "%name%"?'
     }, {
-        name: 'max',
-        type: 'input',
-        prerequisites: {
-            type: 'string'
-        },
-        postrequisites: function (val, properties) {
-            return properties.min ? val > properties.min : val > 0;
-        },
-        required: false,
-        casts: ['integer']
-    }
+		name : 'additional',
+		type: 'list',
+		choices: ['alphanum', 'email', 'lowercase', 'uppercase', 'none'],
+		prerequisites : {
+			type: 'string'
+		},
+		message: 'Select the "additional" property for "%name%":',
+		postrequisites : function(val) {
+			return val !== 'none';
+		},
+		transform : function(val, properties){
+			properties[val] = true;
+			return undefined;
+		}
+	},
+	{
+		name: 'min',
+		prerequisites: or('type', ['string', 'number', 'integer']),
+		postrequisites: function (val) {
+			return val > 0;
+		},
+		required: false,
+		casts: ['integer']
+    },
+	{
+		name: 'max',
+		prerequisites: or('type', ['string', 'number', 'integer']),
+		postrequisites: function (val, properties) {
+			return properties.min ? val > properties.min : val > 0;
+		},
+		required: false,
+		casts: ['integer']
+    },
+	{
+		name: 'length',
+		prerequisites: {
+			type: 'string',
+			min: undefined,
+			max: undefined
+		},
+		casts: ['integer'],
+		postrequisites: function (val, properties) {
+			return val > 0;
+		}
+	},
+	{
+		name: 'enum',
+		prerequisites: {
+			type: 'string',
+			min: undefined,
+			max: undefined,
+			length: undefined,
+			additional : undefined,
+			lowercase: undefined,
+			uppercase: undefined,
+			email: undefined,
+			alphanum: undefined
+		},
+		message: 'What are the "enum" allowed values for "%name%"?',
+		postrequisites : function(val){
+			return val.split(',').length > 1;
+		},
+		transform: function(val, def){
+			var toret = _.map(val.split(','), _.trim);
+			console.log(val, toret);
+			return toret;
+		}
+	},
+	{
+		name: 'min',
+		prerequisites: {
+			type: 'date'
+		},
+		casts: ['date']
+	}, {
+		name: 'max',
+		prerequisites: {
+			type: 'date'
+		},
+		casts: ['date'],
+		postrequisites: function (val, properties) {
+			return properties.min ? val > properties.min : true;
+		}
+	}
 ];

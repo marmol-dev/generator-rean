c3b569d88debfa1aefb5632ef014da2005ee9f2d
tomymolina <tm07081995@gmail.com>
2015-08-15 17:05:31 +0200

    Solved a typo in the "config/db" path

--- crud-module/templates/express-module/_.server.controller.js
+++ crud-module/templates/express-module/_.server.controller.js
@@ -6,7 +6,7 @@
 var errorHandler = require('./errors.server.controller'),
 	<%= classifiedSingularName %> = require('../models/<%= slugifiedSingularName %>.server.model'),
 	_ = require('lodash'),
-        r = require('../../confib/db').getThinky().r;
+    r = require('../../config/db').getThinky().r;
 
 exports.cleanInput = function(req, res, next) {
 	//remove "non-editable" attributes from req.body  <% for (var name in nonEditableAttributes) { %>
